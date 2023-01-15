e940aea7b6a2cb43edadc86f4d5b94f87f576b04
Eric Guo <eric.guocz@gmail.com>
2023-01-15 13:42:32 +0800

    allow all feature.

--- app/models/enterprise_token.rb
+++ app/models/enterprise_token.rb
@@ -38,11 +38,11 @@ class EnterpriseToken < ApplicationRecord
     end
 
     def allows_to?(action)
-      Authorization::EnterpriseService.new(current).call(action).result
+      true
     end
 
     def show_banners?
-      OpenProject::Configuration.ee_manager_visible? && (!current || current.expired?)
+      false
     end
 
     def set_current_token
@@ -80,7 +80,7 @@ class EnterpriseToken < ApplicationRecord
   end
 
   def allows_to?(action)
-    Authorization::EnterpriseService.new(self).call(action).result
+    true
   end
 
   def unset_current_token
