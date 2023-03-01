8b2cc85da80a972f51d5a682a9121c2fdcc6a29f
Eric-Guo <eric.guocz@gmail.com>
2023-03-01 22:36:40 +0800

    New theme "Thape Light" and "Thape Dark"

--- lib/open_project/custom_styles/color_themes.rb
+++ lib/open_project/custom_styles/color_themes.rb
@@ -65,7 +65,7 @@ module OpenProject::CustomStyles
           'header-item-bg-hover-color' => "#E1E1E1",
           'header-item-font-color' => "#313131",
           'header-item-font-hover-color' => "#313131",
-          'header-border-bottom-color' => "#E1E1E1",
+          'header-border-bottom-color' => "",
           'main-menu-bg-color' => "#ECECEC",
           'main-menu-bg-selected-background' => "#A9A9A9",
           'main-menu-bg-hover-background' => "#FFFFFF",
@@ -75,6 +75,50 @@ module OpenProject::CustomStyles
           'main-menu-border-color' => "#EAEAEA"
         },
         logo: 'logo_openproject.png'
+      },
+      {
+        theme: 'Thape Dark',
+        colors: {
+          'primary-color' => "#012C6D",
+          'primary-color-dark' => "#001873",
+          'alternative-color' => "#175A8E",
+          'content-link-color' => "#171F38",
+          'header-bg-color' => "#171F38",
+          'header-item-bg-hover-color' => "#475974",
+          'header-item-font-color' => "#9B9A9A",
+          'header-item-font-hover-color' => "#E0EFF6",
+          'header-border-bottom-color' => "",
+          'main-menu-bg-color' => "#171F38",
+          'main-menu-bg-selected-background' => "#425167",
+          'main-menu-bg-hover-background' => "#475974",
+          'main-menu-font-color' => "#FFFFFF",
+          'main-menu-hover-font-color' => "#39C1F5",
+          'main-menu-selected-font-color' => "#E0EFF6",
+          'main-menu-border-color' => "#001873"
+        },
+        logo: 'logo_ppm_dark.png'
+      },
+      {
+        theme: 'Thape Light',
+        colors: {
+          'primary-color' => "#131E37",
+          'primary-color-dark' => "#1E3144",
+          'alternative-color' => "#01156C",
+          'content-link-color' => "#01156C",
+          'header-bg-color' => "#FBFBFB",
+          'header-item-bg-hover-color' => "#E6EBF9",
+          'header-item-font-color' => "#87898C",
+          'header-item-font-hover-color' => "#196CE8",
+          'header-border-bottom-color' => "#E6EBF9",
+          'main-menu-bg-color' => "#F6F8F9",
+          'main-menu-bg-selected-background' => "#E6EBF9",
+          'main-menu-bg-hover-background' => "#E6EBF9",
+          'main-menu-font-color' => "#01156C",
+          'main-menu-hover-font-color' => "#196CE8",
+          'main-menu-selected-font-color' => "#041E91",
+          'main-menu-border-color' => "#F0F3FE"
+        },
+        logo: 'logo_ppm.png'
       },
       {
         theme: 'OpenProject Dark',
