ce84ade13044e70534e4187301fa25af1276b0cd
秦嘉鹏 <759848795@qq.com>
2023-03-27 17:59:47 +0800

    在项目成员模型中添加profile的属性

--- modules/th_members/lib/open_project/th_members/patches/member_patch.rb
+++ modules/th_members/lib/open_project/th_members/patches/member_patch.rb
@@ -5,10 +5,23 @@ module OpenProject::ThMembers
 
       base.class_eval do
         has_one :profile, class_name: "MemberProfile", foreign_key: "member_id"
+
+        accepts_nested_attributes_for :profile
+
+        after_save :set_profile
+
+        attr_accessor :company, :position, :remark
       end
     end
 
     module InstanceMethods
+      def set_profile
+        profile = self.profile || MemberProfile.new(member_id: self.id)
+        profile.company = company if company.present?
+        profile.position = position if position.present?
+        profile.remark = remark if remark.present?
+        profile.save! if profile.changed?
+      end
     end
   end
 end
