ad1979c0be0fc3e2d657ad41c2b11255c1a4be3e
Eric Guo <eric.guocz@gmail.com>
2023-04-12 18:05:52 +0800

    To resolve phased-restart in production is slow.

--- config/puma_prod.rb
+++ config/puma_prod.rb
@@ -10,11 +10,11 @@ pidfile "/var/www/open_project/shared/tmp/pids/puma.pid"
 state_path "/var/www/open_project/shared/tmp/pids/puma.state"
 stdout_redirect '/var/www/open_project/shared/log/puma_access.log', '/var/www/open_project/shared/log/puma_error.log', true
 
-threads 4, 16
+threads 5, 16
 
 bind 'unix:///var/www/open_project/shared/tmp/sockets/puma.sock'
 
-workers 0
+workers 7
 
 restart_command 'bundle exec puma'
 
