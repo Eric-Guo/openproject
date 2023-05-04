8f6f1ce4f8cc0f59016bd1f35f55e1225e090c47
秦嘉鹏 <qinjiapeng@thape.com.cn>
2023-05-04 11:53:22 +0800

    修复设置全局角色时model的project不存在报错

--- app/services/members/set_attributes_service.rb
+++ app/services/members/set_attributes_service.rb
@@ -68,7 +68,7 @@ module Members
     end
 
     def set_profile_attributes(params)
-      unless model.project.module_enabled?('th_members')
+      unless model.project.present? && model.project.module_enabled?('th_members')
         params.delete(:profile_attributes)
       end
 
