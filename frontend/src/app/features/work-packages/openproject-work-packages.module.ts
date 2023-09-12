37095f9c2f62764aee4689897b12b818dd9a1509
秦嘉鹏 <qinjiapeng@thape.com.cn>
2023-09-12 14:07:48 +0800

    新增同步父级工作包参与者功能

--- frontend/src/app/features/work-packages/openproject-work-packages.module.ts
+++ frontend/src/app/features/work-packages/openproject-work-packages.module.ts
@@ -383,6 +383,7 @@ import {
 import {
   OpBaselineLegendsComponent,
 } from 'core-app/features/work-packages/components/wp-baseline/baseline-legends/baseline-legends.component';
+import { WorkPackageWatchersTabButtonGroupComponent } from './components/wp-single-view-tabs/watchers-tab/watchers-tab-button-group.component';
 import { NgSelectModule } from '@ng-select/ng-select';
 import {
   WorkPackageTimerButtonComponent,
@@ -583,6 +584,7 @@ import {
     // Watchers wp-tab-wrapper
     WorkPackageWatchersTabComponent,
     WorkPackageWatcherEntryComponent,
+    WorkPackageWatchersTabButtonGroupComponent,
 
     // Relations
     WorkPackageRelationsTabComponent,
