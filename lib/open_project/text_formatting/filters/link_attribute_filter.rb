4f34d94cb29bfd51285609d2fcecf577ece08fe6
秦嘉鹏 <qinjiapeng@thape.com.cn>
2023-09-20 13:54:28 +0800

    设置编辑器链接在新标签页打开

--- lib/open_project/text_formatting/filters/link_attribute_filter.rb
+++ lib/open_project/text_formatting/filters/link_attribute_filter.rb
@@ -31,7 +31,7 @@ module OpenProject::TextFormatting
     class LinkAttributeFilter < HTML::Pipeline::Filter
       def call
         links.each do |node|
-          node['target'] = context[:target] if context[:target].present?
+          node['target'] = '_blank'
         end
 
         doc
