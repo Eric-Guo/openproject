a4fe542b7f7abc8ddd365dd27f744e2cd046e157
秦嘉鹏 <qinjiapeng@thape.com.cn>
2023-09-20 11:19:05 +0800

    修改通知默认设置

--- app/seeders/development_data/users_seeder.rb
+++ app/seeders/development_data/users_seeder.rb
@@ -82,7 +82,7 @@ module DevelopmentData
         user.status = User.statuses[:active]
         user.language = I18n.locale.to_s
         user.force_password_change = false
-        user.notification_settings.build(assignee: false, responsible: false, mentioned: true, watched: false, due_date: 1)
+        user.notification_settings.build(assignee: false, responsible: false, mentioned: true, watched: false, start_date: nil, due_date: 1)
       end
     end
   end
