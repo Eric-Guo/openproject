81a077971adc0c2b7008da164a9fa2af46ca29a2
秦嘉鹏 <qinjiapeng@thape.com.cn>
2023-10-30 11:40:54 +0800

    新增工作包文件栏显示数量

--- frontend/src/app/features/work-packages/components/wp-tabs/services/wp-tabs/wp-edoc-files-count.function.ts
+++ frontend/src/app/features/work-packages/components/wp-tabs/services/wp-tabs/wp-edoc-files-count.function.ts
@@ -1,24 +1,15 @@
 import { Injector } from '@angular/core';
-import { combineLatest, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 import { WorkPackageResource } from 'core-app/features/hal/resources/work-package-resource';
-import { FileLinksResourceService } from 'core-app/core/state/file-links/file-links.service';
-import { AttachmentsResourceService } from 'core-app/core/state/attachments/attachments.service';
+import { WorkPackageEdocFilesResourceService } from 'core-app/core/state/work-package-edoc-files/work-package-edoc-files.service';
 
 export function workPackageEdocFilesCount(
   workPackage:WorkPackageResource,
   injector:Injector,
 ):Observable<number> {
-  const attachmentService = injector.get(AttachmentsResourceService);
-  const fileLinkService = injector.get(FileLinksResourceService);
-
-  return combineLatest(
-    [
-      attachmentService.collection(workPackage.$links.attachments.href || ''),
-      fileLinkService.collection(workPackage.$links.fileLinks?.href || ''),
-    ],
-  ).pipe(
-    map(([a, f]) => a.length + f.length),
-  );
+  const attachmentService = injector.get(WorkPackageEdocFilesResourceService);
+  return new Observable<number>((ob) => {
+    attachmentService.subscribeFiles(Number(workPackage.id), (list) => ob.next(list.length));
+  });
 }
