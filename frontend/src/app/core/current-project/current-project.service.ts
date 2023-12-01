c6c3d568aa0fddd6f2ded3533f8d00a336d5669d
秦嘉鹏 <qinjiapeng@thape.com.cn>
2023-12-01 16:02:08 +0800

    新增widget - 快捷方式

--- frontend/src/app/core/current-project/current-project.service.ts
+++ frontend/src/app/core/current-project/current-project.service.ts
@@ -42,6 +42,7 @@ export class CurrentProjectService {
   private currentThCode:string|null = null;
   private currentThName:string|null = null;
   private currentThDocLink:string|null = null;
+  private currentThTypeId:number|null = null;
 
   constructor() {
     this.detect();
@@ -91,6 +92,10 @@ export class CurrentProjectService {
     return this.currentThDocLink;
   }
 
+  public get thTypeId():number|null {
+    return this.currentThTypeId;
+  }
+
   public get ddsFolderId():number|null {
     const reg = /^https?:\/\/edoc\.thape\.com\.cn:8022\/index\.html#doc\/enterprise\/(\d+)/;
 
@@ -111,6 +116,7 @@ export class CurrentProjectService {
       this.currentThCode = element.dataset.projectThCode as string;
       this.currentThName = element.dataset.projectThName as string;
       this.currentThDocLink = element.dataset.projectThDocLink as string;
+      this.currentThTypeId = element.dataset.projectThTypeId ? Number(element.dataset.projectThTypeId) : null;
     } else {
       this.currentId = null;
       this.currentName = null;
@@ -118,6 +124,7 @@ export class CurrentProjectService {
       this.currentThCode = null;
       this.currentThName = null;
       this.currentThDocLink = null;
+      this.currentThTypeId = null;
     }
   }
 }
