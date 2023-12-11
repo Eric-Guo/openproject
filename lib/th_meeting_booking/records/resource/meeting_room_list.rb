c3e212e63e6373f28ea426018730055fe70a9008
秦嘉鹏 <qinjiapeng@thape.com.cn>
2023-12-11 15:09:22 +0800

    天华会议模块会议室列表新增排序

--- lib/th_meeting_booking/records/resource/meeting_room_list.rb
+++ lib/th_meeting_booking/records/resource/meeting_room_list.rb
@@ -9,10 +9,15 @@ module ThMeetingBooking::Records::Resource
 
     def data=(value)
       if value.is_a?(Array)
-        @data = value.map { |item| MeetingRoom.new(item) }
+        @data = sort_data(value.map { |item| MeetingRoom.new(item) })
       else
         @data = nil
       end
+    end
+
+    private
+    def sort_data(data)
+      data.group_by(&:office_area).sort.map { |_,items| items.sort_by(&:show_order) }.flatten
     end
   end
 end
