ba75867e3a672e95a1dbc3fe394996bb8425779f
秦嘉鹏 <qinjiapeng@thape.com.cn>
2023-12-22 14:45:32 +0800

    工作包提及邮件标题添加项目名称

--- app/mailers/work_package_mailer.rb
+++ app/mailers/work_package_mailer.rb
@@ -47,6 +47,7 @@ class WorkPackageMailer < ApplicationMailer
       send_localized_mail(recipient) do
         I18n.t(:"mail.mention.subject",
                user_name: author.name,
+               project_name: @work_package.project.name,
                id: @work_package.formatted_id,
                subject: @work_package.subject)
       end
