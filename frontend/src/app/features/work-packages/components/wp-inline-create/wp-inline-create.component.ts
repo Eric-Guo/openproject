9dbfae5e9c0d1f23bb86ad5f4746be56bbd775ef
秦嘉鹏 <qinjiapeng@thape.com.cn>
2024-03-21 15:00:16 +0800

    在工作包列表下方增加导入工作集功能

--- frontend/src/app/features/work-packages/components/wp-inline-create/wp-inline-create.component.ts
+++ frontend/src/app/features/work-packages/components/wp-inline-create/wp-inline-create.component.ts
@@ -57,6 +57,9 @@ import { WorkPackageChangeset } from 'core-app/features/work-packages/components
 import { EditForm } from 'core-app/shared/components/fields/edit/edit-form/edit-form';
 import { UntilDestroyedMixin } from 'core-app/shared/helpers/angular/until-destroyed.mixin';
 import { SchemaCacheService } from 'core-app/core/schemas/schema-cache.service';
+import { WpImportModalComponent } from 'core-app/features/plugins/linked/openproject-th_work_packages/wp-import-modal/wp-import.modal';
+import { OpModalService } from 'core-app/shared/components/modal/modal.service';
+import { InjectField } from 'core-app/shared/helpers/angular/inject-field.decorator';
 import {
   inlineCreateCancelClassName,
   InlineCreateRowBuilder,
@@ -82,6 +85,8 @@ export class WorkPackageInlineCreateComponent extends UntilDestroyedMixin implem
 
   @Output() showing = new EventEmitter<boolean>();
 
+  @InjectField() protected opModalService:OpModalService;
+
   // inner state
   public canAdd = false;
 
@@ -92,7 +97,10 @@ export class WorkPackageInlineCreateComponent extends UntilDestroyedMixin implem
 
   public focus = false;
 
-  public text = this.wpInlineCreate.buttonTexts;
+  public text = {
+    ...this.wpInlineCreate.buttonTexts,
+    import: '导入工作集',
+  };
 
   private currentWorkPackage:WorkPackageResource|null;
 
@@ -215,6 +223,16 @@ export class WorkPackageInlineCreateComponent extends UntilDestroyedMixin implem
 
   public handleAddRowClick() {
     this.addWorkPackageRow();
+    return false;
+  }
+
+  public handleImportRowClick() {
+    this.wpCreate
+      .createOrContinueWorkPackage(this.projectIdentifier)
+      .then((change: WorkPackageChangeset) => {
+        this.currentWorkPackage = change.projectedResource;
+        this.opModalService.show(WpImportModalComponent, this.injector, { workPackage: this.currentWorkPackage });
+      });
     return false;
   }
 
