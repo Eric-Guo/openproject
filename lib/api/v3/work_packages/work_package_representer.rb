e6035120ab0b712f3f3e984e69056e2a63e97ea4
Eric Guo <eric.guocz@gmail.com>
2024-05-22 11:00:33 +0800

    用户在工作包填写工时后，不可删除该工作包

--- lib/api/v3/work_packages/work_package_representer.rb
+++ lib/api/v3/work_packages/work_package_representer.rb
@@ -83,7 +83,8 @@ module API
         end
 
         link :delete,
-             cache_if: -> { current_user.allowed_in_project?(:delete_work_packages, represented.project) || (current_user.allowed_in_project?(:delete_my_create_work_packages, represented.project) && current_user.id == represented.author_id) } do
+             cache_if: -> { (current_user.allowed_in_project?(:delete_work_packages, represented.project) && (represented.time_entries.blank? || current_user.admin?)) \
+              || (current_user.allowed_in_project?(:delete_my_create_work_packages, represented.project) && (current_user.id == represented.author_id) && (represented.time_entries.blank? || current_user.admin?)) } do
           {
             href: api_v3_paths.work_package(represented.id),
             method: :delete
