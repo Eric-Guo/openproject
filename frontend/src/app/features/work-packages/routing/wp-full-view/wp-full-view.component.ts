9b6b0f0e0afbd922138aa41d4f364ac68c84a286
Eric Guo <eric.guocz@gmail.com>
2024-06-13 15:39:42 +0800

    不允许用户在非“天华工作填报”页面中填写已耗工时 17552

--- frontend/src/app/features/work-packages/routing/wp-full-view/wp-full-view.component.ts
+++ frontend/src/app/features/work-packages/routing/wp-full-view/wp-full-view.component.ts
@@ -118,7 +118,6 @@ export class WorkPackagesFullViewComponent extends WorkPackageSingleViewBase imp
   private setWorkPackageScopeProperties(wp:WorkPackageResource) {
     this.isWatched = Object.prototype.hasOwnProperty.call(wp, 'unwatch');
     this.displayWatchButton = Object.prototype.hasOwnProperty.call(wp, 'unwatch') || Object.prototype.hasOwnProperty.call(wp, 'watch');
-    this.displayTimerButton = Object.prototype.hasOwnProperty.call(wp, 'logTime');
     this.displayShareButton$ = this.currentUserService.hasCapabilities$('work_package_shares/index', (wp.project as ProjectResource).id);
     this.displayReminderButton$ = this.currentUserService.isLoggedInAndHasCapabalities$(
       'work_packages/read',
