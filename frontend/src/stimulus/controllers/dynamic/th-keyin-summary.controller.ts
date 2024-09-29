eb07f4c2da06355a04a36ded866fa2d46ac9fedc
Eric Guo <eric.guocz@gmail.com>
2024-09-29 16:41:53 +0800

    th-keyin-summary will do the page hour summury.

---
+++ frontend/src/stimulus/controllers/dynamic/th-keyin-summary.controller.ts
@@ -0,0 +1,79 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class ThKeyinSummaryController extends Controller {
+  static targets = ['drawingHourSum', 'discussingHourSum'];
+
+  declare readonly drawingHourSumTarget:HTMLSpanElement;
+  declare readonly discussingHourSumTarget:HTMLSpanElement;
+
+  private drawing_inputs:HTMLInputElement[];
+  private discussing_inputs:HTMLInputElement[];
+  private user_keyin_data_but_not_save:boolean;
+  private keyin_form:HTMLFormElement = document.getElementById('th-keyin-form') as HTMLFormElement;
+
+  connect() {
+    this.drawing_inputs = Array.from(this.element.querySelectorAll('input[name="time_entry[today_drawing_entry][]"]'));
+    this.discussing_inputs = Array.from(this.element.querySelectorAll('input[name="time_entry[today_discussing_entry][]"]'));
+
+    this.calculateDrawingHour();
+    this.calculateDiscussingHour();
+    this.user_keyin_data_but_not_save = false;
+
+    this.drawing_inputs.forEach((input:HTMLInputElement) => {
+      input.addEventListener('input', this.calculateDrawingHour.bind(this));
+    });
+    this.discussing_inputs.forEach((input:HTMLInputElement) => {
+      input.addEventListener('input', this.calculateDiscussingHour.bind(this));
+    });
+    window.addEventListener('beforeunload', this.handleBeforeUnload.bind(this));
+    this.keyin_form.addEventListener('submit', this.handleSubmit.bind(this));
+  }
+
+  disconnect() {
+    this.drawing_inputs.forEach((input:HTMLInputElement) => {
+      input.removeEventListener('input', this.calculateDrawingHour.bind(this));
+    });
+    this.discussing_inputs.forEach((input:HTMLInputElement) => {
+      input.removeEventListener('input', this.calculateDiscussingHour.bind(this));
+    });
+    window.removeEventListener('beforeunload', this.handleBeforeUnload.bind(this));
+    this.keyin_form.removeEventListener('submit', this.handleSubmit.bind(this));
+  }
+
+  calculateDrawingHour() {
+    let sum = 0;
+    this.drawing_inputs.forEach((input:HTMLInputElement) => {
+      const value = parseFloat(input.value);
+      if (!Number.isNaN(value)) {
+        sum += value;
+      }
+    });
+    this.drawingHourSumTarget.textContent = `${I18n.t('js.labour_keyin.drawing_hour')}: ${sum.toFixed(1)}`;
+    this.user_keyin_data_but_not_save = true;
+  }
+
+  calculateDiscussingHour() {
+    let sum = 0;
+    this.discussing_inputs.forEach((input:HTMLInputElement) => {
+      const value = parseFloat(input.value);
+      if (!Number.isNaN(value)) {
+        sum += value;
+      }
+    });
+    this.discussingHourSumTarget.textContent = `${I18n.t('js.labour_keyin.discussing_hour')}: ${sum.toFixed(1)}`;
+    this.user_keyin_data_but_not_save = true;
+  }
+
+  private handleBeforeUnload(event:BeforeUnloadEvent) {
+    if (this.user_keyin_data_but_not_save) {
+      event.preventDefault();
+      // Custom message (note: modern browsers will ignore this and show their own message)
+      event.returnValue = I18n.t('js.labour_keyin.leave_without_save');
+      return event.returnValue;
+    }
+  }
+
+  private handleSubmit(_event:SubmitEvent) {
+    this.user_keyin_data_but_not_save = false;
+  }
+}
