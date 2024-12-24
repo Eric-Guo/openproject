2b36fca07cacffb0116ff244a282da1cc9360202
Eric Guo <eric.guocz@gmail.com>
2024-12-24 14:47:23 +0800

    th-keyin-summary add travel hours

--- frontend/src/stimulus/controllers/dynamic/th-keyin-summary.controller.ts
+++ frontend/src/stimulus/controllers/dynamic/th-keyin-summary.controller.ts
@@ -1,22 +1,26 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class ThKeyinSummaryController extends Controller {
-  static targets = ['drawingHourSum', 'discussingHourSum'];
+  static targets = ['drawingHourSum', 'discussingHourSum', 'travelHourSum'];
 
   declare readonly drawingHourSumTarget:HTMLSpanElement;
   declare readonly discussingHourSumTarget:HTMLSpanElement;
+  declare readonly travelHourSumTarget:HTMLSpanElement;
 
   private drawing_inputs:HTMLInputElement[];
   private discussing_inputs:HTMLInputElement[];
+  private travel_inputs:HTMLInputElement[];
   private user_keyin_data_but_not_save:boolean;
   private keyin_form:HTMLFormElement = document.getElementById('th-keyin-form') as HTMLFormElement;
 
   connect() {
     this.drawing_inputs = Array.from(this.element.querySelectorAll('input[name="time_entry[today_drawing_entry][]"]'));
     this.discussing_inputs = Array.from(this.element.querySelectorAll('input[name="time_entry[today_discussing_entry][]"]'));
+    this.travel_inputs = Array.from(this.element.querySelectorAll('input[name="time_entry[today_travel_entry][]"]'));
 
     this.calculateDrawingHour();
     this.calculateDiscussingHour();
+    this.calculateTravelHour();
     this.user_keyin_data_but_not_save = false;
 
     this.drawing_inputs.forEach((input:HTMLInputElement) => {
@@ -24,6 +28,9 @@ export default class ThKeyinSummaryController extends Controller {
     });
     this.discussing_inputs.forEach((input:HTMLInputElement) => {
       input.addEventListener('input', this.calculateDiscussingHour.bind(this));
+    });
+    this.travel_inputs.forEach((input:HTMLInputElement) => {
+      input.addEventListener('input', this.calculateTravelHour.bind(this));
     });
     window.addEventListener('beforeunload', this.handleBeforeUnload.bind(this));
     this.keyin_form.addEventListener('submit', this.handleSubmit.bind(this));
@@ -35,6 +42,9 @@ export default class ThKeyinSummaryController extends Controller {
     });
     this.discussing_inputs.forEach((input:HTMLInputElement) => {
       input.removeEventListener('input', this.calculateDiscussingHour.bind(this));
+    });
+    this.travel_inputs.forEach((input:HTMLInputElement) => {
+      input.removeEventListener('input', this.calculateTravelHour.bind(this));
     });
     window.removeEventListener('beforeunload', this.handleBeforeUnload.bind(this));
     this.keyin_form.removeEventListener('submit', this.handleSubmit.bind(this));
@@ -61,6 +71,18 @@ export default class ThKeyinSummaryController extends Controller {
       }
     });
     this.discussingHourSumTarget.textContent = `${I18n.t('js.labour_keyin.discussing_hour')}: ${sum.toFixed(1)}`;
+    this.user_keyin_data_but_not_save = true;
+  }
+
+  calculateTravelHour() {
+    let sum = 0;
+    this.travel_inputs.forEach((input:HTMLInputElement) => {
+      const value = parseFloat(input.value);
+      if (!Number.isNaN(value)) {
+        sum += value;
+      }
+    });
+    this.travelHourSumTarget.textContent = `${I18n.t('js.labour_keyin.travel_hour')}: ${sum.toFixed(1)}`;
     this.user_keyin_data_but_not_save = true;
   }
 
