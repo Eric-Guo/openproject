f8bfa0ec336bd08f2a7b778fd97a1743a4b03104
Eric Guo <eric.guocz@gmail.com>
2025-01-16 14:26:54 +0800

    当用户在团队空间中的工作包列表中右键点击创建子工作包，若点击的工作包属于的项目和当前工作表项目不一致，则不允许创建子工作包
    (cherry picked from commit e992e48304ec441228e42e470b8e873451b715d5)

--- frontend/src/app/features/work-packages/components/wp-table/context-menu-helper/wp-context-menu-helper.service.ts
+++ frontend/src/app/features/work-packages/components/wp-table/context-menu-helper/wp-context-menu-helper.service.ts
@@ -31,6 +31,7 @@ import { HalLink } from 'core-app/features/hal/hal-link/hal-link';
 import { Injectable, inject } from '@angular/core';
 import { PathHelperService } from 'core-app/core/path-helper/path-helper.service';
 import { UrlParamsHelperService } from 'core-app/features/work-packages/components/wp-query/url-params-helper';
+import { CurrentProjectService } from 'core-app/core/current-project/current-project.service';
 import { HookService } from 'core-app/features/plugins/hook-service';
 import { WorkPackageViewTimelineService } from 'core-app/features/work-packages/routing/wp-view-base/view-services/wp-view-timeline.service';
 import { WorkPackageViewHierarchyIdentationService } from 'core-app/features/work-packages/routing/wp-view-base/view-services/wp-view-hierarchy-indentation.service';
@@ -54,6 +55,7 @@ export class WorkPackageContextMenuHelperService {
   private wpViewTimeline = inject(WorkPackageViewTimelineService);
   private wpViewIndent = inject(WorkPackageViewHierarchyIdentationService);
   private PathHelper = inject(PathHelperService);
+  private currentProject = inject(CurrentProjectService);
 
   private BULK_ACTIONS = [
     {
@@ -231,7 +233,11 @@ export class WorkPackageContextMenuHelperService {
       });
     }
 
-    if (!!workPackage.addChild && allowSplitScreenActions) {
+    const workPackageInCurrentProject = workPackage.project.id === this.currentProject.id;
+    const currentProjectInGroupSpace = this.currentProject.thTypeId === 1;
+    const thAllowAddChild = currentProjectInGroupSpace ? workPackageInCurrentProject : true;
+
+    if (!!workPackage.addChild && thAllowAddChild && allowSplitScreenActions) {
       allowedActions.push({
         key: 'relation-new-child',
         text: I18n.t('js.relation_buttons.add_new_child'),
