279576aba53e1f61e936670c2393f74dfeec0f2f
Eric Guo <eric.guocz@gmail.com>
2025-03-18 10:47:05 +0800

    Prompt: Please add access control to /api/v3/users/100 and disallow anonymous user access.

--- lib/api/v3/users/users_api.rb
+++ lib/api/v3/users/users_api.rb
@@ -82,6 +82,10 @@ module API
                 else
                   User.visible.find_by_unique!(params[:id]) # rubocop:disable Rails/DynamicFindBy
                 end
+
+              authorize_by_with_raise(current_user.logged?) do
+                raise API::Errors::Unauthenticated
+              end unless [55].include?(@user.id)
             end
 
             get &::API::V3::Utilities::Endpoints::Show.new(model: User).mount
