bfa99f155f66abd50a2218dc3d98dd209c6c73f8
Eric Guo <eric.guocz@gmail.com>
2025-03-18 13:48:34 +0800

    在项目状态列表里增加“已暂停”状态

--- app/constants/projects/statuses.rb
+++ app/constants/projects/statuses.rb
@@ -39,6 +39,7 @@ module Projects
     ON_TRACK = Status.new(code: :on_track, color: Color.new(hexcode: "#1F883D"), icon: "issue-opened")
     AT_RISK = Status.new(code: :at_risk, color: Color.new(hexcode: "#BC4C00"), icon: "alert")
     OFF_TRACK = Status.new(code: :off_track, color: Color.new(hexcode: "#CF222E"), icon: "stop")
+    ON_PAUSE = Status.new(code: :on_pause, color: Color.new(hexcode: "#0969DA"), icon: "pause")
     NOT_STARTED = Status.new(code: :not_started, color: Color.new(hexcode: "#0969DA"), icon: "circle")
     FINISHED = Status.new(code: :finished, color: Color.new(hexcode: "#8250DF"), icon: "issue-closed")
     DISCONTINUED = Status.new(code: :discontinued, color: Color.new(hexcode: "#9A6700"), icon: "no-entry")
@@ -47,6 +48,7 @@ module Projects
       ON_TRACK,
       AT_RISK,
       OFF_TRACK,
+      ON_PAUSE,
       NOT_STARTED,
       FINISHED,
       DISCONTINUED
