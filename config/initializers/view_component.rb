91f8bf9e6887e4d791f9bf072f8b1f9e9457f9af
Eric-Guo <eric.guocz@gmail.com>
2025-08-30 10:57:36 +0800

    Allow view_components

--- config/initializers/view_component.rb
+++ config/initializers/view_component.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   config.view_component.generate.preview_path = Rails.root.join("spec/components/previews").to_s
-  config.view_component.preview_paths << Rails.root.join("spec/components/previews").to_s
+  config.view_component.previews.paths << Rails.root.join("spec/components/previews").to_s
 
   # Enable instrumentation (e.g., for AppSignal)
   config.view_component.instrumentation_enabled = true
