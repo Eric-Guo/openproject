fd19fcd77112727a8e417b3b11bf849861493137
Eric-Guo <eric.guocz@gmail.com>
2025-09-03 21:42:09 +0800

    Remove 微件：我花费的时间, may also need remove in DB.
    DELETE FROM grid_widgets
    USING grids
    WHERE grids.id = grid_widgets.grid_id
      AND grids.type = 'Grids::MyPage'
      AND grid_widgets.identifier = 'time_entries_current_user'

---
+++ modules/my_page/db/migrate/20260418120000_remove_time_entries_current_user_widgets.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+# -- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+# ++
+
+class RemoveTimeEntriesCurrentUserWidgets < ActiveRecord::Migration[8.0]
+  def up
+    remove_time_entries_current_user_widgets
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+
+  private
+
+  def remove_time_entries_current_user_widgets
+    execute <<~SQL.squish
+      DELETE FROM grid_widgets
+      USING grids
+      WHERE grids.id = grid_widgets.grid_id
+        AND grids.type = 'Grids::MyPage'
+        AND grid_widgets.identifier = 'time_entries_current_user'
+    SQL
+  end
+end
