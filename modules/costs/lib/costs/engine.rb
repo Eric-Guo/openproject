8fb568704b96490a635ceb86ff21fe9e898c2e60
Eric-Guo <eric.guocz@gmail.com>
2025-09-03 20:33:41 +0800

    Limit allow /my/time_tracking to view_cost_entries

--- modules/costs/lib/costs/engine.rb
+++ modules/costs/lib/costs/engine.rb
@@ -112,7 +112,7 @@ module Costs
                    require: :member
 
         permission :view_cost_entries,
-                   { costlog: [:index] },
+                   { costlog: [:index], "my/time_tracking": [:index] },
                    permissible_on: :project
         permission :view_own_cost_entries,
                    { costlog: [:index] },
@@ -155,7 +155,8 @@ module Costs
            after: :my_page,
            caption: :label_my_time_tracking,
            if: ->(*) do
-             User.current.allowed_in_any_project?(:log_own_time) || User.current.allowed_in_any_project?(:log_time)
+             (User.current.allowed_in_any_project?(:log_own_time) || User.current.allowed_in_any_project?(:log_time)) &&
+               (User.current.admin? || User.current.allowed_globally?(:view_all_project_info))
            end,
            icon: :stopwatch
 
@@ -166,7 +167,8 @@ module Costs
            context: :my,
            caption: :label_my_time_tracking,
            if: ->(*) do
-             User.current.allowed_in_any_project?(:log_own_time) || User.current.allowed_in_any_project?(:log_time)
+             (User.current.allowed_in_any_project?(:log_own_time) || User.current.allowed_in_any_project?(:log_time)) &&
+               (User.current.admin? || User.current.allowed_globally?(:view_all_project_info))
            end,
            icon: :stopwatch
     end
