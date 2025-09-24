6c56e0825b54b1e6473a0c9fbacd6691ef14ab25
Eric-Guo <eric.guocz@gmail.com>
2025-09-24 14:33:20 +0800

    ✨When first time load, user need click twice to close the welcome_text, please fix it.
    Promote the my page.
    Display total number.
    Another links.

--- app/controllers/concerns/accounts/redirect_after_login.rb
+++ app/controllers/concerns/accounts/redirect_after_login.rb
@@ -48,7 +48,7 @@ module Accounts::RedirectAfterLogin
     if (url = Setting.after_login_default_redirect_url)
       redirect_back_or_default url
     else
-      redirect_back_or_default home_path
+      redirect_back_or_default my_page_url
     end
   end
 
