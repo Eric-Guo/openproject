abc176e0dfbcb3c271e5773c253f680ff9264c12
Eric-Guo <eric.guocz@gmail.com>
2026-04-21 23:10:01 +0800

    Do auto deploy to ai_dev
    (cherry picked from commit 2f36f191871b3978585932aa2cb464bc02df8cca)

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 set :application, "open_project"
 set :user, "open_project"
 set :nginx_use_ssl, true
-set :branch, :new_prod
+set :branch, :ai_prod
 set :rails_env, "production"
 set :puma_service_unit_name, :puma_plm
 set :puma_systemctl_user, :system
