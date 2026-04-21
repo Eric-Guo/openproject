76e7edcb6b3da93e4da4f220b4abc00d2d6cfa18
Eric-Guo <eric.guocz@gmail.com>
2026-04-22 00:07:24 +0800

    Add directly

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -80,9 +80,7 @@ Rails.application.config.after_initialize do
         connect_src += ["https://appsignal-endpoint.net"]
       end
 
-      if ENV["CSP_CONNECT_SRC"].present?
-        connect_src << ENV["CSP_CONNECT_SRC"]
-      end
+      connect_src << "https://analytics.thape.com.cn"
 
       # Allow connections to S3 for BIM
       if OpenProject::Configuration.fog_directory.present?
