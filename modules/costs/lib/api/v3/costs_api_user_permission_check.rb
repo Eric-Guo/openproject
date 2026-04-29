d5d699d901834dfa2652b32b6334c6f3f9f7be97
Eric-Guo <eric.guocz@gmail.com>
2026-04-29 17:43:36 +0800

    Gate spent time link by project permissions

--- modules/costs/lib/api/v3/costs_api_user_permission_check.rb
+++ modules/costs/lib/api/v3/costs_api_user_permission_check.rb
@@ -21,6 +21,12 @@ module API::V3::CostsApiUserPermissionCheck
     view_time_entries_allowed?
   end
 
+  def spent_time_report_visible?
+    current_user.admin? ||
+      current_user.allowed_globally?(:view_all_project_info) ||
+      current_user.allowed_in_project?(:view_th_budget, represented.project)
+  end
+
   private
 
   # Unit costs (material, overall total and costs by type) are meaningless without
