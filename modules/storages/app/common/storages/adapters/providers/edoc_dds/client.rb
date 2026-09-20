68b96d142c2914f8a3e3400a8ae79600eeaa7d6d
Eric-Guo <eric.guocz@gmail.com>
2026-09-20 22:56:39 +0800

    Harden EDoc DDS client API handling

--- modules/storages/app/common/storages/adapters/providers/edoc_dds/client.rb
+++ modules/storages/app/common/storages/adapters/providers/edoc_dds/client.rb
@@ -40,6 +40,8 @@ module Storages
       module EdocDds
         class Client
           MAX_CHUNK_SIZE = 5.megabytes
+          FILE_PAGE_SIZE = 100
+          REQUEST_TIMEOUT = 30
 
           class Error < StandardError
             attr_reader :code, :payload
@@ -59,109 +61,72 @@ module Storages
             result = get("/api/services/Folder/GetChildrenFolders", token_param.merge(topFolderId: folder_id))
             assert_result!(result)
 
-            Array(result[:data]).map do |item|
+            result.fetch(:data).map do |item|
               {
-                folder_id: item[:FolderId],
-                folder_name: item[:FolderName]
+                folder_id: item[:folder_id],
+                folder_name: item[:folder_name]
               }
             end
           end
 
           def list_files(folder_id)
-            result = get("/api/services/File/GetChildFileListByFolderId", token_param.merge(folderId: folder_id))
-            assert_result!(result)
+            files = []
+            page = 1
 
-            Array(result[:data]).map do |item|
-              {
-                file_id: item[:FileId],
-                file_name: item[:FileName],
-                ext_name: item[:FileExtName],
-                size: item[:FileLastSize] || item[:FileSize] || 0
-              }
+            loop do
+              result = file_page(folder_id, page)
+              files.concat(result[:files].map { |item| listed_file(item) })
+              break if page * FILE_PAGE_SIZE >= result[:total_count]
+
+              page += 1
             end
+
+            files
           end
 
-          def folder_info(folder_id) # rubocop:disable Metrics/AbcSize
+          def folder_info(folder_id)
             result = get("/api/services/Folder/GetFolderInfoById", token_param.merge(folderId: folder_id))
             assert_result!(result)
 
-            data = result[:data]
-            {
-              child_file_count: data[:ChildFileCount],
-              child_folder_count: data[:ChildFolderCount],
-              create_time: data[:CreateTime],
-              creator_id: data[:CreatorId],
-              creator_name: data[:CreatorName],
-              editor_id: data[:EditorId],
-              editor_name: data[:EditorName],
-              folder_id: data[:FolderId],
-              folder_name: data[:FolderName],
-              folder_path: data[:FolderPath],
-              folder_size: data[:FolderSize],
-              modify_time: data[:ModifyTime],
-              parent_folder_id: data[:ParentFolderId],
-              permission: data[:Permission]
-            }
+            result.fetch(:data).slice(:child_file_count, :child_folder_count, :create_time, :creator_id, :creator_name,
+                                      :editor_id, :editor_name, :folder_id, :folder_name, :folder_path, :folder_size,
+                                      :modify_time, :parent_folder_id, :permission)
           end
 
-          def file_info(file_id) # rubocop:disable Metrics/AbcSize
+          def file_info(file_id)
             result = get("/api/services/File/GetFileInfoById", token_param.merge(fileId: file_id))
             assert_result!(result)
 
-            data = result[:data]
-            {
-              file_id: data[:FileId],
-              file_name: data[:FileName],
-              file_size: data[:FileSize],
-              file_modify_time: data[:FileModifyTime],
-              editor_name: data[:EditorName],
-              file_create_time: data[:FileCreateTime],
-              creator_id: data[:CreatorId],
-              creator_name: data[:CreatorName],
-              parent_folder_id: data[:ParentFolderId],
-              file_path: data[:FilePath],
-              file_ext_name: data[:FileExtName],
-              can_preview: data[:CanPreview],
-              can_download: data[:CanDownload],
-              can_delete_file: data[:CanDeleteFile]
-            }
+            result.fetch(:data).slice(:file_id, :file_name, :file_size, :file_modify_time, :editor_name,
+                                      :file_create_time, :creator_id, :creator_name, :parent_folder_id,
+                                      :file_path, :file_ext_name, :can_preview, :can_download, :can_delete_file)
           end
 
           def create_folder(parent_folder_id:, name:)
             result = post_json(
               "/api/services/Folder/CreateFolder",
-              token_param(:Token).merge(
-                ParentFolderId: parent_folder_id,
-                Name: name,
-                FolderCode: "",
-                Remark: ""
+              token_param.merge(
+                parentFolderId: parent_folder_id,
+                name:,
+                folderCode: "",
+                remark: ""
               )
             )
 
-            raise Error.new(result.inspect, code: :conflict, payload: result) unless [0, 806].include?(result[:result])
+            assert_result!(result, success_codes: [0, 806])
 
             {
-              folder_id: result.dig(:data, :FolderId),
-              folder_name: result.dig(:data, :Name)
+              folder_id: result.dig(:data, :folder_id),
+              folder_name: result.dig(:data, :name)
             }
           end
 
           def remove_folder(folder_id)
-            result = post_json(
-              "/api/services/Doc/RemoveFolderListAndFileList",
-              token_param.merge(FolderIdList: [folder_id])
-            )
-            assert_result!(result)
-            result[:data]
+            remove_documents(folder_ids: [folder_id])
           end
 
           def remove_file(file_id)
-            result = post_json(
-              "/api/services/Doc/RemoveFolderListAndFileList",
-              token_param.merge(FileIdList: [file_id])
-            )
-            assert_result!(result)
-            result[:data]
+            remove_documents(file_ids: [file_id])
           end
 
           def upload(input_io, folder_id:, file_name:)
@@ -195,13 +160,51 @@ module Storages
           end
 
           def download_url(file_id)
-            result = get("/downLoad/DownLoadCheck", token_param.merge(fileIds: file_id))
-            raise Error.new(result.inspect, code: :error, payload: result) unless result[:nResult].to_i.zero?
+            result = get("/downLoad/DownLoadCheck", token_param.merge(file_id:))
+            raise Error, "EDoc download check failed" unless result[:n_result] == 0
 
-            build_url("/downLoad/index", token_param.merge(file_id:, regionHash: result[:RegionHash]))
+            build_url("/downLoad/index", token_param.merge(file_id:, regionHash: result[:region_hash]))
           end
 
           private
+
+          def file_page(folder_id, page)
+            result = get("/api/services/File/GetChildFilePageListByFolderId",
+                         token_param.merge(folderId: folder_id, pageIndex: page, pageSize: FILE_PAGE_SIZE))
+            assert_result!(result)
+            validate_file_page!(result, page)
+            result
+          end
+
+          def validate_file_page!(result, page)
+            total = result[:total_count]
+            unless total.is_a?(Integer) && total >= 0 && result[:files].is_a?(Array)
+              raise Error, "EDoc returned an invalid file page"
+            end
+
+            if result[:files].empty? && (page - 1) * FILE_PAGE_SIZE < total
+              raise Error, "EDoc returned an empty page before the end of the file list"
+            end
+          end
+
+          def listed_file(item)
+            {
+              file_id: item[:file_id],
+              file_name: item[:file_name],
+              ext_name: item[:file_ext_name],
+              size: item[:file_last_size] || item[:file_size] || 0
+            }
+          end
+
+          def remove_documents(file_ids: [], folder_ids: [])
+            result = post_json(
+              "/api/services/Doc/RemoveFolderListAndFileList",
+              token_param.merge(fileIdList: file_ids, folderIdList: folder_ids,
+                                filePathList: [], folderPathList: [], async: false)
+            )
+            assert_result!(result)
+            result[:data]
+          end
 
           def start_upload(path, folder_id:, file_name:) # rubocop:disable Metrics/AbcSize
             file_size = File.size(path)
@@ -232,15 +235,15 @@ module Storages
               md5:,
               file_size:,
               file_name:,
-              file_id: result.dig(:data, :FileId),
-              file_ver_id: result.dig(:data, :FileVerId),
-              folder_id: result.dig(:data, :ParentFolderId),
-              region_hash: result.dig(:data, :RegionHash),
-              region_id: result.dig(:data, :RegionId),
-              region_type: result.dig(:data, :RegionType),
-              region_url: result.dig(:data, :RegionUrl),
+              file_id: result.dig(:data, :file_id),
+              file_ver_id: result.dig(:data, :file_ver_id),
+              folder_id: result.dig(:data, :parent_folder_id),
+              region_hash: result.dig(:data, :region_hash),
+              region_id: result.dig(:data, :region_id),
+              region_type: result.dig(:data, :region_type),
+              region_url: result.dig(:data, :region_url),
               upload_id: SecureRandom.uuid,
-              second_pass: truthy?(result[:secondPass])
+              second_pass: truthy?(result[:second_pass])
             }
           end
 
@@ -285,8 +288,8 @@ module Storages
               file: path
             )
 
-            raise Error.new(result.inspect, code: :error, payload: result) if result[:status] == "Error"
-            raise Error.new("Upload cancelled", code: :error, payload: result) if result[:status] == "Cancel"
+            raise Error, "EDoc chunk upload failed" if result[:status] == "Error"
+            raise Error, "Upload cancelled" if result[:status] == "Cancel"
 
             result
           end
@@ -321,35 +324,50 @@ module Storages
             end
           end
 
-          def perform_request(uri_or_url, request_or_class) # rubocop:disable Metrics/AbcSize
+          def perform_request(uri_or_url, request_or_class)
             uri = uri_or_url.is_a?(URI) ? uri_or_url : URI(uri_or_url)
             request = request_or_class.is_a?(Class) ? request_or_class.new(uri) : request_or_class
 
-            response = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == "https") do |http|
+            response = Net::HTTP.start(uri.host, uri.port, **http_options(uri)) do |http|
               http.request(request)
             end
 
             return response if response.is_a?(Net::HTTPSuccess)
 
-            raise Error.new(response.body, code: http_error_code(response), payload: response)
-          rescue SystemCallError, SocketError, OpenSSL::SSL::SSLError, Timeout::Error, URI::InvalidURIError => e
-            raise Error.new(e.message, code: :error, payload: e)
+            raise Error.new("EDoc HTTP request failed (#{response.code})", code: http_error_code(response))
+          rescue Timeout::Error
+            raise Error, "EDoc request timed out"
+          rescue SystemCallError, SocketError, OpenSSL::SSL::SSLError, IOError, URI::InvalidURIError
+            raise Error, "EDoc connection failed"
+          end
+
+          def http_options(uri)
+            options = { use_ssl: uri.scheme == "https" }
+            return options unless uri.path.include?("/api/services/")
+
+            options.merge(open_timeout: REQUEST_TIMEOUT, read_timeout: REQUEST_TIMEOUT,
+                          write_timeout: REQUEST_TIMEOUT, max_retries: 0)
           end
 
           def parse_response(response)
-            JSON.parse(response.body.to_s).with_indifferent_access
-          rescue JSON::ParserError => e
-            raise Error.new(e.message, code: :error, payload: e)
+            result = JSON.parse(response.body.to_s)
+            raise Error, "EDoc returned an invalid response" unless result.is_a?(Hash)
+
+            result.deep_transform_keys { |key| key.underscore.to_sym }
+          rescue JSON::ParserError
+            raise Error, "EDoc returned invalid JSON"
           end
 
-          def assert_result!(result)
-            return if result[:result].to_i.zero?
+          def assert_result!(result, success_codes: [0])
+            codes = result.values_at(:result, :code).compact
+            return if codes.any? && codes.all? { |code| success_codes.include?(code) }
 
-            raise Error.new(result.inspect, code: error_code(result), payload: result)
+            code = codes.find { |value| success_codes.exclude?(value) }
+            raise Error.new("EDoc API request failed (result #{code.inspect})", code: error_code(code))
           end
 
-          def error_code(result)
-            case result[:result].to_i
+          def error_code(code)
+            case code
             when 610
               :conflict
             else
@@ -377,8 +395,8 @@ module Storages
             "#{Setting.protocol}://#{Setting.host_name}"
           end
 
-          def token_param(key = :token)
-            { key => @storage.token }
+          def token_param
+            { token: @storage.token }
           end
 
           def with_file_path(input_io)
