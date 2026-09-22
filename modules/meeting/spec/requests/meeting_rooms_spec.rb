6868d2311b5ebc015716f3133bf91def02d18a8c
Eric-Guo <eric.guocz@gmail.com>
2026-09-22 17:08:38 +0800

    Test meeting room changes and synchronization failures

--- modules/meeting/spec/requests/meeting_rooms_spec.rb
+++ modules/meeting/spec/requests/meeting_rooms_spec.rb
@@ -33,7 +33,7 @@ require "spec_helper"
 RSpec.describe "Meeting room selection", :skip_csrf, type: :rails_request do
   shared_let(:project) { create(:project, enabled_module_names: %i[meetings]) }
   shared_let(:user) { create(:user, member_with_permissions: { project => %i[view_meetings edit_meetings] }) }
-  shared_let(:meeting) do
+  let(:meeting) do
     build(:meeting, project:, author: user, state: :draft, start_time: 1.day.ago).tap do |meeting|
       meeting.save!(validate: false)
     end
@@ -111,5 +111,54 @@ RSpec.describe "Meeting room selection", :skip_csrf, type: :rails_request do
     expect(meeting.reload.th_meeting_upstream_room_id).to eq(room.id)
     expect(meeting.th_meeting_id).to eq("booking-1")
     expect(ThMeetingBooking::Apis::Booking).to have_received(:sync_meetings).once
+  end
+
+  it "shows booking errors and preserves the saved room when synchronization fails" do
+    meeting.update_column(:start_time, 1.day.from_now)
+    meeting.th_meeting.update_columns(upstream_room_id: "room-2", th_meeting_id: "booking-1")
+    allow(ThMeetingBooking::Apis::Booking).to receive(:sync_meetings).and_raise(StandardError, "Booking update failed")
+
+    put update_details_project_meeting_path(project, meeting),
+        params: { meeting: { th_meeting_upstream_room_id: room.id } },
+        as: :turbo_stream
+
+    expect(response).to have_http_status(:bad_request)
+    dialog = Nokogiri::HTML.fragment(response.body)
+    expect(dialog.text).to include("Booking update failed")
+    expect(dialog.at_css('button[type="submit"]')["disabled"]).to be_nil
+    expect(meeting.reload.th_meeting_upstream_room_id).to eq("room-2")
+    expect(meeting.th_meeting_id).to eq("booking-1")
+  end
+
+  {
+    "online to physical" => ["线上会议", "Shanghai"],
+    "physical to online" => ["Shanghai", "线上会议"],
+    "physical to physical" => ["Shanghai", "Shanghai"]
+  }.each do |description, (original_area, target_area)|
+    it "changes rooms from #{description}" do
+      original_room = instance_double(ThMeetingBooking::Records::Resource::MeetingRoom,
+                                      id: "original-room", office_area_id: "original-area",
+                                      office_area: original_area, name: "Original room")
+      target_room = instance_double(ThMeetingBooking::Records::Resource::MeetingRoom,
+                                    id: "target-room", office_area_id: "target-area",
+                                    office_area: target_area, name: "Target room")
+      rooms = [original_room, target_room]
+      allow(ThMeeting).to receive(:available_rooms).and_return(rooms)
+      allow(ThMeetingBooking::Apis::Resource).to receive(:meeting_rooms).and_return(Struct.new(:data).new(rooms))
+      allow(ThMeetingBooking::Apis::Booking).to receive(:sync_meetings).and_return(Struct.new(:id).new("booking-1"))
+      meeting.update!(start_time: 1.day.from_now, th_meeting_upstream_room_id: original_room.id)
+      updated_meeting = Meeting.find(meeting.id)
+
+      put update_details_project_meeting_path(project, meeting),
+          params: { meeting: { th_meeting_upstream_room_id: target_room.id } },
+          as: :turbo_stream
+
+      expect(response).to have_http_status(:ok)
+      expect(updated_meeting.reload.th_meeting_upstream_room_id).to eq(target_room.id)
+      expect(updated_meeting.location).to eq("#{target_area} - Target room")
+      expect(updated_meeting.th_meeting_id).to eq("booking-1")
+      expect(ThMeetingBooking::Apis::Booking).to have_received(:sync_meetings)
+        .with(hash_including(upstream_room_id: target_room.id)).once
+    end
   end
 end
